e9b3c945cb76332ecd76d6e9aea0e021299bb39f
GuiCon5k <guilletheswager1@gmail.com>
2022-08-10 19:25:19 -0300

    Cambios en el router

--- Router.php
+++ Router.php
@@ -22,7 +22,6 @@ class Router {
         $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar',
         '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
 
-
         $urlActual = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $metodo = $_SERVER['REQUEST_METHOD'];
         
@@ -30,6 +29,19 @@ class Router {
             $fn = $this->rutasGET[$urlActual] ?? null;
         } else {
             $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
+        $method = $_SERVER['REQUEST_METHOD'];
+            
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+        // debuguear($splitURL);
+        
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        } else {
+        $fn = $this->postRoutes[$splitURL[0]] ?? null;
         }
 
         //Proteger las rutas
